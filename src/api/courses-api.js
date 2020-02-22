1c28e0581cdd98047aa4f88c3676cc49916ef40f
Kareem <khaled.kareem0506@gmail.com>
2020-02-22 18:39:56 -0500

    adding api calls for fetchAllCourses, fetchCourse, createReview

--- src/api/courses-api.js
+++ src/api/courses-api.js
@@ -9,3 +9,22 @@ let axiosConfig = {
 };
   
 
+function fetchAllCourses(data) {
+    return axios.get(`${BASE_URL}/api/GetCourseData/`, data, axiosConfig);
+}
+
+function fetchCourse(data) {
+    return axios.get(`${BASE_URL}/api/GetCourseDetails?courseid=${data.courseId}/`, data, axiosConfig);
+}
+
+
+// expected data: { "selections": [ 1234567, 0101010, 5564732, 1238921 ] }
+function createReview(data) {
+    return axios.post(`${BASE_URL}/api/SubmitCourseSelections/`, data, axiosConfig);
+}
+
+export {
+    fetchAllCourses,
+    fetchCourse,
+    createReview,
+};
