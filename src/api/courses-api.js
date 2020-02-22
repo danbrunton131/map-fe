0fda8b912a5537607a27a7f2779159d0c12775d4
Kareem <khaled.kareem0506@gmail.com>
2020-02-22 18:20:39 -0500

    adding base url to variables file, to indicate where the BE url is located. Adding api file with axiosConfig

---
+++ src/api/courses-api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import {BASE_URL} from '../variables';
+
+let axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json',
+        // "Access-Control-Allow-Origin": "*",
+    },
+};
+  
+
---
+++ src/sample_variables.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'http://localhost:5000;'
+
+export {
+    BASE_URL,
+}
