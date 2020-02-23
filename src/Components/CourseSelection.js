e07cef693b4fa955a2bc58161fdea2fc312c60ee
Kareem <khaled.kareem0506@gmail.com>
2020-02-22 21:45:56 -0500

    collecting all courses within a season. BE input should be simplified...this is a workaround

--- src/Components/CourseSelection.js
+++ src/Components/CourseSelection.js
@@ -1,7 +1,7 @@
 import '../css/course-selection.css';
 import React from 'react';
 import {Col, Row, Tabs, Tab} from 'react-bootstrap';
-import {fetchAllCourses} from '../../api/courses-api';
+import {fetchAllCourses} from '../api/courses-api';
 
 const generateCourseList = (allCourses, addCourseToCart) => {
     return allCourses.map((course, index) => {
@@ -21,7 +21,18 @@ const generateCourseList = (allCourses, addCourseToCart) => {
       );
     })
   }
-
+  // BE input should be provided in a simpler way:  {Fall: [all courses], Winter: [all courses]}
+  // This is a workaround
+  const getAllSeasonCourses = (seasonCourses) => {
+    let allCourses = [];
+    for (let classType in seasonCourses){ 
+      const classTypeList = seasonCourses[classType];
+      for (let i=0; i<classTypeList.length;i++){ //i is the course itself
+        allCourses.push(classTypeList[i]);
+      }
+    }
+    return allCourses;
+  }
 
 export default class CourseSelection extends React.Component {
     constructor(props) {
@@ -33,8 +44,10 @@ export default class CourseSelection extends React.Component {
 
     componentDidMount(){
       fetchAllCourses().then(res => {
-        this.setState({fallCourseList: res.data});
-        console.log(res.data);
+        console.log(res.data.courseLists.Fall);
+        const fallCourses = getAllSeasonCourses(res.data.courseLists.Fall);
+        this.setState({fallCourseList: fallCourses});
+        console.log(fallCourses);
       })
       .catch((err) => {
         console.log("AXIOS ERROR: ", err);
--- src/sample_variables.js
+++ src/sample_variables.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000;'
+const BASE_URL = 'http://localhost:8000';
 
 export {
     BASE_URL,
