74a68948875c137000cd2503d04148d02cae9f12
Kareem <khaled.kareem0506@gmail.com>
2020-02-29 15:24:01 -0500

    showing Loading text until course list loads

--- src/Components/CourseSelection.js
+++ src/Components/CourseSelection.js
@@ -1,6 +1,6 @@
 import '../css/course-selection.css';
 import React from 'react';
-import {Col, Row, Tabs, Tab, Nav} from 'react-bootstrap';
+import { Tab, Nav} from 'react-bootstrap';
 
 const generateCourseList = (allCourses, addCourseToCart) => {
     return allCourses.map((course, index) => {
@@ -35,38 +35,40 @@ const generateCourseList = (allCourses, addCourseToCart) => {
     render() {
       const {allCourses, addCourseToCart} = this.props;
       const {currentSeason} = this.state;
-      console.log(allCourses);
-        return(
-            <div className="course-selection">
-                <h2> Course Selection </h2>
 
-                <Tab.Container id="tab-bar" defaultActiveKey="fall">
-                    <div className="navbar navbar-expand-md navigation-menu">
-                      <Nav className="navbar-nav nav nav-pills flex-column flex-lg-row flex-md-row" onSelect={this.seasonChange.bind(this)}>
-                        <Nav.Item>
-                          <Nav.Link className="nav-item nav-link" eventKey="fall" title="Fall">Fall</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                          <Nav.Link className="nav-item nav-link" eventKey="winter" title="Winter">Winter</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                          <Nav.Link className="nav-item nav-link" eventKey="spring" title="Spring">Spring</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                          <Nav.Link className="nav-item nav-link" eventKey="summer" title="Summer">Summer</Nav.Link>
-                        </Nav.Item>
-                      </Nav>
-                    </div>
-                    <Tab.Content>
-                      <Tab.Pane className="tab-pane" eventKey={currentSeason}>
-                        {allCourses[currentSeason] && allCourses[currentSeason].length > 0 ?
-                            generateCourseList(allCourses[currentSeason], addCourseToCart) 
-                        : <div> There are no courses available for this Term.</div>}
-                      </Tab.Pane>
-                    </Tab.Content>
-                </Tab.Container>
-            </div>
-        );
-    }
+      return(
+          <div className="course-selection">
+              <h2> Course Selection </h2>
+              <Tab.Container id="tab-bar" defaultActiveKey="fall">
+                  <div className="navbar navbar-expand-md navigation-menu">
+                    <Nav className="navbar-nav nav nav-pills flex-column flex-lg-row flex-md-row" onSelect={this.seasonChange.bind(this)}>
+                      <Nav.Item>
+                        <Nav.Link className="nav-item nav-link" eventKey="fall" title="Fall">Fall</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link className="nav-item nav-link" eventKey="winter" title="Winter">Winter</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link className="nav-item nav-link" eventKey="spring" title="Spring">Spring</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link className="nav-item nav-link" eventKey="summer" title="Summer">Summer</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                  </div>
+                  <Tab.Content>
+                    <Tab.Pane className="tab-pane" eventKey={currentSeason}>
+                      {/* show loading text until course list loads. */}
+                      { !allCourses[currentSeason] ? <div> Loading... </div> :
+                        allCourses[currentSeason].length > 0 ?
+                          generateCourseList(allCourses[currentSeason], addCourseToCart) 
+                        : <div> There are no courses available for this Term.</div>
+                      }
+                    </Tab.Pane>
+                  </Tab.Content>
+              </Tab.Container>
+          </div>
+      );
   }
+}
   
