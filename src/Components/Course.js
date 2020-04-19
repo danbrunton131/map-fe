b23305893d2ce7ebe3feeb72481115f7e5c217c5
Kareem <khaled.kareem0506@gmail.com>
2020-04-18 22:58:56 -0400

    adding + icon for adding to cart, fixing DOM order of icons

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -2,7 +2,7 @@ import '../css/course-selection.css';
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faInfoCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default class Course extends React.Component {
     constructor(props) {
@@ -33,18 +33,33 @@ export default class Course extends React.Component {
           >
           {course.courseCode} 
           </span>
-          {/*Add clickability to expand course to show info about course*/}
-          {/*onKeyPress={(e) => e.key === "Enter" && showCourseInfo(course.courseID)}
-          onClick={() => showCourseInfo(course.courseID)}*/}
-          <div
-              tabIndex={0}
-              className="float-right"
-              onKeyPress={(e) => e.key === "Enter" && this.toggleOpen(isOpen)}
-              onClick={() => this.toggleOpen(isOpen)}
-              aria-label="Expand Course Information"
-          >
-            <FontAwesomeIcon icon={faAngleDown} size="xs" />
-          </div>
+          {/* Course Action Icons for Description and AddToCart*/}
+          <span className="course-actions float-right"> {/* ensures DOM ordering  of icons is preserved for tabbability */}
+            <div
+                tabIndex={0}
+                className="float-left mr-3"
+                id={course.courseID}
+                name={course.name}
+                onClick={!course.selected ? () => addCourseToCart(course.courseID) : null}
+                onKeyPress={!course.selected ? (e) => e.key === "Enter" && addCourseToCart(course.courseID) : null}
+                aria-label="Add course to cart"
+            >
+              <FontAwesomeIcon icon={faPlusCircle} size="xs" />
+            </div>
+            {/*Add clickability to expand course to show info about course*/}
+            {/*onKeyPress={(e) => e.key === "Enter" && showCourseInfo(course.courseID)}
+            onClick={() => showCourseInfo(course.courseID)}*/}
+            <div
+                tabIndex={0}
+                className="float-left mr-3"
+                onKeyPress={(e) => e.key === "Enter" && this.toggleOpen(isOpen)}
+                onClick={() => this.toggleOpen(isOpen)}
+                aria-label="Expand Course Information"
+            >
+              <FontAwesomeIcon icon={faAngleDown} size="xs" />
+            </div>
+          </span>
+
           {
             isOpen === true && <div>Course Description stuff</div>
           }    
