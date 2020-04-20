b804d766555071c9ccbd6d9455dfec4519963711
Kareem <khaled.kareem0506@gmail.com>
2020-04-20 18:39:04 -0400

    updating width for description to fit space

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -65,8 +65,8 @@ export default class Course extends React.Component {
           </div>
 
           {/* Course Description */}
-          <SmoothCollapse expanded={isOpen}>
-            <p className="course-description" tabIndex={0}>
+          <SmoothCollapse expanded={isOpen} className="course-description">
+            <p tabIndex={0}>
             <strong>Course Title </strong>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
