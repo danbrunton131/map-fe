acee6d1c1251eef8ca33ef2be5c46cc6c6946e4b
Kareem <khaled.kareem0506@gmail.com>
2020-04-20 22:16:51 -0400

    preventing course description from causing icons to move

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -77,14 +77,6 @@ export default class Course extends React.Component {
               >
                 <FontAwesomeIcon icon={isOpen ? faAngleUp: faAngleDown } size="1x"/>
               </div>
-
-              {/* Course Description */}
-              <SmoothCollapse expanded={isOpen} className="course-description">
-                <p tabIndex={0}>
-                <strong>Course Title </strong>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-              </SmoothCollapse>
             </React.Fragment>
           }
 
@@ -103,6 +95,15 @@ export default class Course extends React.Component {
             </div>
           }
           </div>
+
+          {/* Course Description */}
+          <SmoothCollapse expanded={isOpen} className="course-description">
+            <p tabIndex={0}>
+            <strong>Course Title </strong>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </SmoothCollapse>
+
         </div>
       );
     }
