b60473212c400fc80c2be5a1400d282c90184af2
Kareem <khaled.kareem0506@gmail.com>
2020-04-22 22:41:26 -0400

    submitting input from searchform to backend

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -8,13 +8,15 @@ export default class SearchBar extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        isOpen: 'false',
+        searchTerm: '',
       };
       this.submitSearch = this.submitSearch.bind(this);
+      this.updateSearchForm = this.updateSearchForm.bind(this);
     }
 
     submitSearch() {
-      searchForCourse({searchTerm: "MATH"}).then(res => {
+      const {searchTerm} = this.state;
+      searchForCourse({searchTerm}).then(res => {
         console.log(res)
       // this.setState({allCourses});
       // console.log(allCourses);
@@ -22,19 +24,29 @@ export default class SearchBar extends React.Component {
       .catch((err) => {
         console.log("AXIOS ERROR: ", err);
     });
+  }
 
+    updateSearchForm(e) {
+      console.log(e.target.value);
+      this.setState({searchTerm: e.target.value});
     }
 
-    toggleOpen(isOpen) {
-      this.setState({isOpen:!isOpen});
-    }
 
     render() {
+      const {searchTerm} = this.state;
+
       return(
         <React.Fragment>
             <h2> Search </h2>
             <InputGroup size="md" className="mb-3">
-                <FormControl type="text" className="search-input" placeholder="SEARCH FOR A COURSE..." aria-labelledby="Search for a course"/>
+                <FormControl
+                 type="text"
+                 className="search-input"
+                 placeholder="SEARCH FOR A COURSE..." 
+                 defaultValue={searchTerm} 
+                 onChange={this.updateSearchForm}
+                 aria-labelledby="Search for a course"
+                />
                 <InputGroup.Append>
                     <button className="btn btn-secondary btn-search" onClick={this.submitSearch}></button>
                 </InputGroup.Append>
