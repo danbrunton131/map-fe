63accd2403656117878300ea06cc45c868debad2
Kareem <khaled.kareem0506@gmail.com>
2020-04-23 18:12:27 -0400

    refactoring searching process

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -17,10 +17,12 @@ export default class SearchBar extends React.Component {
       this.state = {
         searchTerm: '',
         results: [],
-        message: '',
+        errorMessage: '',
         searching: false,
+        isSearchValid: null,
       };
       this.submitSearch = this.submitSearch.bind(this);
+      this.validateAndSubmitSearch = this.validateAndSubmitSearch.bind(this);
       this.updateSearchForm = this.updateSearchForm.bind(this);
       this.handleClickOutside = this.handleClickOutside.bind(this);
     }
@@ -29,53 +31,58 @@ export default class SearchBar extends React.Component {
     componentWillUnmount() { document.removeEventListener("click", this.handleClickOutside, false); }
     
     submitSearch() {
-      const {searchTerm, isValid} = this.state;
-      this.setState({searching: true}, 
-        () => {
-            if (isValid) {
-              searchForCourse({searchTerm}).then(res => {
-                console.log(res);
-                if (res.data.error && res.data.error === "long query"){
-                  this.setState({message: "Your request was too long", results: []});
-                } else if (res.data.results && res.data.results.length === 0){
-                  this.setState({message: "No results found", results: []});
-                } else {
-                  this.setState({results: res.data.results , message:""});
-                }
-              }).catch((err) => {
-                console.log("AXIOS ERROR: ", err);
-              })
-          }});
-  }
+      const {searchTerm} = this.state;
+      searchForCourse({searchTerm}).then(res => {
+        // console.log(res);
+        if (res.data.error && res.data.error === "long query"){
+          this.setState({errorMessage: "Your request was too long", results: []});
+        } else if (res.data.results && res.data.results.length === 0){
+          this.setState({errorMessage: "No results found", results: []});
+        } else {
+          this.setState({results: res.data.results , errorMessage:""});
+        }
+      }).catch((err) => {
+        console.log("AXIOS ERROR: ", err);
+      });
+    } 
 
+    // before submitting form, set validation state and enable searching mode
+    validateAndSubmitSearch() {
+      const {searchTerm} = this.state;
+      const isSearchValid = searchTerm.length > 0 && searchTerm.length <= 30;
+
+      this.setState({searching: true, isSearchValid},
+        ()=> {
+          if (isSearchValid) {
+            this.submitSearch();
+          }
+      });
+    }
+
+    // We perform a live search after updating the search term
     updateSearchForm(e) {
       this.setState(
-        { 
-          searchTerm: e.target.value,
-          isValid: e.target.value.length > 0 && e.target.value.length <= 30,
-        }, 
-        this.submitSearch() // Live Searching - submit after updating search term
-      ); 
+        { searchTerm: e.target.value},
+         () => this.validateAndSubmitSearch());
     }
 
     handleClickOutside(e){
       if (this.searchBox.contains(e.target)) {
         return;
       }
-      this.setState({results: [], message: "", searching: false});
+      this.setState({results: [], errorMessage: "", searching: false});
     }
 
     // Allow user to submit search via the enter button
     enterToSubmit(e){
       if(e.key === "Enter"){
-          this.submitSearch(); 
+          this.validateAndSubmitSearch(); 
       }
       return false;
     }
 
     render() {
-      const {searchTerm, results, message, searching, isValid} = this.state;
-      console.log(isValid);
+      const {searchTerm, results, errorMessage, searching, isSearchValid} = this.state;
       return(
         <div className="search-bar">
             <h2> Search </h2>
@@ -88,24 +95,26 @@ export default class SearchBar extends React.Component {
                  onChange={this.updateSearchForm}
                  aria-labelledby="Search for a course"
                  onKeyPress={this.enterToSubmit.bind(this)}
-                //  isInvalid={searching && !isValid}
+                 isInvalid={isSearchValid===false}
                 >
                 </FormControl>
                 <InputGroup.Append>
-                    <button className="btn btn-secondary btn-search" onClick={this.submitSearch}></button>
+                    <button className="btn btn-secondary btn-search" disabled={isSearchValid===false} onClick={this.validateAndSubmitSearch}></button>
                 </InputGroup.Append>
-                { !isValid && searching && <FormControl.Feedback type="invalid">Your search must be between 0 and 31 characters</FormControl.Feedback>}
-
-                { isValid && searching && 
-                  <div className="search-results-container">
-                    <div className="search-results">
-                    {results.length > 0 ? generateSearchResults(results, this.props.addCourseToCart) : message}
-                    </div>
-                  </div>
-                } 
-
+                  {searching && 
+                    <React.Fragment>
+                      { isSearchValid ? 
+                        <div className="search-results-container">
+                          <div className="search-results">
+                            {results.length > 0 ? generateSearchResults(results, this.props.addCourseToCart) : errorMessage}
+                          </div>
+                        </div>
+                        : <FormControl.Feedback type="invalid">Your search must be between 0 and 31 characters</FormControl.Feedback> 
+                      }
+                    </React.Fragment> 
+                  }
             </InputGroup>
-      </div>
-);
+        </div>
+      );
     }
-  }
+}
