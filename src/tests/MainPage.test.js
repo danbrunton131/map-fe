f04f299b831b5dbd41534b1bebb60be58fd71221
Kareem <khaled.kareem0506@gmail.com>
2020-04-27 03:52:08 -0400

    replacing and accessibilizing loading spinner to deal with failing test case due to a specific library

--- src/tests/MainPage.test.js
+++ src/tests/MainPage.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+// import { shallow } from 'enzyme';
 
 import {getTermCourseList} from '../Components/MainPage.js';
+
 
 
 const seasonCourses = {
