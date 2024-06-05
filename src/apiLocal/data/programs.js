d3e131bbd2ea3e10552e7d7f7e199b328ef75a89
thompsdc <50490931+thompsdc@users.noreply.github.com>
2024-06-05 10:37:21 -0400

    Back To Previous version...something is not working
    copied and pasted from a previous version to see why MAPS isn't working

--- src/apiLocal/data/programs.js
+++ src/apiLocal/data/programs.js
@@ -241,9 +241,22 @@ const programs = [
     ]
 },
 {
+    id: "HLSCISMSYS",
+    name: "Honours Life Sciences — Sensory Motor Systems Specialization ",
+    slug: "",
+    requirements: [
+        { count: 1, from: ["BIOLOGY 1A03"] },
+        { count: 1, from: ["CHEM 1A03"] },
+        { count: 1, from: ["MATH 1A03", "MATH 1LS3"] },
+        { count: 1, from: ["PHYSICS 1A03", "PHYSICS 1C03"] },
+        { count: 1, from: ["BIOLOGY 1M03", "EARTHSC 1G03", "ENVIRSC 1C03", "PSYCH 1FF3", "PSYCH 1XX3"] },
+        { count: 3, from: scienceOneCourseList }
+    ]
+},
+{
     id: "HMATHCSCI",
     name: "Honours Mathematics & Computer Science",
-    slug: "honours-mathematics-and-computer-science",
+    slug: "honours-life-sciences-sensory-motor-systems-specialization",
     requirements: [
         { count: 1, from: firstTermMath },
         { count: 1, from: secondTermMath },
@@ -368,7 +381,7 @@ const programs = [
         { count: 2, from: ["BIOLOGY 1A03", "BIOLOGY 1M03"] },
         { count: 1, from: ["PSYCH 1FF3", "PSYCH 1XX3"] },
         { count: 1, from: ["MATH 1A03", "MATH 1LS3"] },
-        { count: 1, from: ["BIOPHYS 1S03", "CHEM 1A03", "PHYSICS 1A03", "PHYSICS 1C03"] },
+        { count: 1, from: ["PHYSICS 1A03", "PHYSICS 1C03"] },
         { count: 3, from: scienceOneCourseList }
     ]
 },
@@ -427,7 +440,6 @@ const programs = [
     requirements: [
         { count: 2, from: [...firstTermMath, ...secondTermMath] },
         { count: 1, from: ["COMPSCI 1MD3", "MATH 1B03", "MATH 1MP3"]}
-        { count: 2, from: scienceOneCourseList }
     ]
 }
 ];
