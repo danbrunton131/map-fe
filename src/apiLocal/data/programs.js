fc4bdb66a938cc2ff6944f61401cbfe2bf8084b8
thompsdc <50490931+thompsdc@users.noreply.github.com>
2024-06-05 10:46:24 -0400

    Updated Mathematical Sciences Course Requirements
    Added: { count: 2, from: scienceOneCourseList }

--- src/apiLocal/data/programs.js
+++ src/apiLocal/data/programs.js
@@ -440,6 +440,7 @@ const programs = [
     requirements: [
         { count: 2, from: [...firstTermMath, ...secondTermMath] },
         { count: 1, from: ["COMPSCI 1MD3", "MATH 1B03", "MATH 1MP3"]}
+        { count: 2, from: scienceOneCourseList }
     ]
 }
 ];
