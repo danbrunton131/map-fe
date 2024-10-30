f036a76803564ec954396278961af6b50d322068
thompsdc <50490931+thompsdc@users.noreply.github.com>
2024-10-30 14:20:19 -0400

    Update programs.js
     Honours Life Sciences Origins of Disease Specialization.
    
    Please adjust so that it requires: 6/6 units from BIOLOGY 1A03, BIOLOGY 1M03
    
    Remove BIOLOGY 1M03 from: 3/3 units from BIOLOGY 1M03, EARTHSC 1G03, ENVIRSC 1C03, PSYCH 1FF3, PSYCH 1XX3

--- src/apiLocal/data/programs.js
+++ src/apiLocal/data/programs.js
@@ -232,11 +232,11 @@ const programs = [
     name: "Honours Life Sciences — Origins of Disease Specialization",
     slug: "honours-life-sciences-orgins-of-desease-specialization",
     requirements: [
-        { count: 1, from: ["BIOLOGY 1A03"] },
+        { count: 2, from: ["BIOLOGY 1A03", "BIOLOGY 1M03"] },
         { count: 2, from: ["CHEM 1A03", "CHEM 1AA3"] },
         { count: 1, from: ["MATH 1A03", "MATH 1LS3"] },
         { count: 1, from: ["PHYSICS 1A03", "PHYSICS 1C03"] },
-        { count: 1, from: ["BIOLOGY 1M03", "EARTHSC 1G03", "ENVIRSC 1C03", "PSYCH 1FF3", "PSYCH 1XX3"] },
+        { count: 1, from: ["EARTHSC 1G03", "ENVIRSC 1C03", "PSYCH 1FF3", "PSYCH 1XX3"] },
         { count: 2, from: scienceOneCourseList }
     ]
 },
