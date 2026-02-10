303d99c5669275af5cbddfe979ebeeb2479579d5
thompsdc <50490931+thompsdc@users.noreply.github.com>
2026-02-10 10:14:00 -0500

    Changed Bio 1A03 description.js
    BIOLOGY 1A02 & 1A01 when offered and taken together are considered equivalent to BIOLOGY 1A03

--- src/apiLocal/data/courses.js
+++ src/apiLocal/data/courses.js
@@ -21,7 +21,7 @@ const courses = [
             "fall": true,
             "winter": true
         },
-        "timetable": "Three hours (lectures, web modules), one lab (three hours, alternating weeks), one tutorial (one hour, alternating weeks); one term"
+        "timetable": "Three hours (lectures, web modules), one lab (three hours, alternating weeks), one tutorial (one hour, alternating weeks), BIOLOGY 1A02 & 1A01 when offered and taken together are considered equivalent to BIOLOGY 1A03 ; one term"
     },
     {
         "code": "BIOLOGY 1M03",
